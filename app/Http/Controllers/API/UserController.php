35ffaf56fd340bcaf33c0974e89865d232bff65f
perdanaph <danangputro9@gmail.com>
2023-01-21 19:13:21 +0700

    feat: create controller user for update || bug in validation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
@@ -43,6 +45,76 @@ class UserController extends Controller
                 'message' => 'Something went wrong',
                 'error' => $error,
             ], 'Fail', 500);
+        }
+    }
+
+    public function login(Request $request) {
+        try {
+            $request->validate([
+                'email'=>'email|required',
+                'password'=>'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            
+            $user = User::where('email', $request->email)->first();
+            
+            if (!Hash::check($request->password, $user->password, [])) {
+                return ResponFormatter::error([
+                    'message' => 'Wrong Password',
+                    
+                ], 'Fail', 400);
+            }
+            if (!Auth::attempt($credentials)) {
+                return ResponFormatter::error([
+                    'message' => 'Field Email & Password Requeired',
+                    
+                ], 'Fail, BadRequest', 400);
+            }
+            $token = $user->createToken('authToken')->plainTextToken;
+            return ResponFormatter::success([
+                'token' => $token,
+                'token type' => 'Bearer ',
+                'user' => $user
+            ], 'Success Authentication');
+        } catch (Exception $error) {
+            return ResponFormatter::error([
+                'message'=>'Internal Server Error',
+                'error'=>$error
+            ], 'Failed', 500);
+        }
+    }
+
+    public function fetch(Request $request) {
+        return ResponFormatter::success($request->user(), 'Success to get data Users');
+    }
+
+    public function update(Request $request) {
+        try {
+            // $validation = Validator::make($request->all(), [
+            //     'name' => 'required',
+            //     'email' => 'required | email',
+            //     'username' => 'required',
+            //     'phone' => 'nullable'
+            // ]);
+
+            // if ($validation->fails()) {
+            //     return ResponFormatter::error([
+            //     ], 'name, email, username canot be omitted', 400);
+            // }
+
+            $data = $request->all();
+            $user = Auth::user();
+            $user->update($data);
+
+            return ResponFormatter::success([
+                $user
+            ], 'Profile Success Updated');
+
+        } catch (Exception $error) {
+            return ResponFormatter::error([
+                'error' => $error
+            ], 'Internal Server Error', 500);
         }
     }
 }
